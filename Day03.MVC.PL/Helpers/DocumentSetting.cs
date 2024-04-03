5f4e5f93422d12a5fb5993326880620f37b436a7
kamal <ka856248@gmail.com>
2024-04-03 06:58:23 +0200

    Documentation Setting

---
+++ Day03.MVC.PL/Helpers/DocumentSetting.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace Day03.MVC.PL.Helpers
+{
+	public static class DocumentSetting
+	{
+		public static string UploadFile(IFormFile file,string FolderName)
+		{
+			//1.Get Located folder path
+			//string FolderPath = $"C:\\rout\\MVC\\Session03\\Pdf\\MVC.Solution\\Day03.MVC.PL\\wwwroot\\Files\\{FolderName}";
+			//string FolderPath = $"{Directory.GetCurrentDirectory()}\\wwwroot\\Files\\{{FolderName}}"; 
+			string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", FolderName);  //Recommended
+
+			if(!Directory.Exists(folderPath)) 
+				Directory.CreateDirectory(folderPath);
+
+			//2.Get File Name and Make it UNIQUE
+
+			string fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+			//3.File Path => folder Path + File Name	
+			string filePath=Path.Combine(folderPath, fileName);
+
+			//4.Save Files as Streams [Data Per Time]
+			var fileStream=new FileStream(filePath,FileMode.Create); 
+			file.CopyTo(fileStream);
+			return fileName;
+			
+		}
+		public static void Deletefile(string fileName,string FolderName)
+		{
+			string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", FolderName, fileName);
+
+			if(File.Exists(filePath))
+				File.Delete(filePath); 
+		}
+
+	}
+}
