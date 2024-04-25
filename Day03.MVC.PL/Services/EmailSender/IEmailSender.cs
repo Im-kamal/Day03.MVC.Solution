4979196f1a67d1f37de353ea916279923c0ba41d
kamal <ka856248@gmail.com>
2024-04-25 09:27:30 +0200

    Email Sender

---
+++ Day03.MVC.PL/Services/EmailSender/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Day03.MVC.PL.Services.EmailSender
+{
+	public interface IEmailSender
+	{
+		Task SendAsync(string form,string recipients,string subject,string body);
+	}
+}
